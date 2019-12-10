49f6f97693446ec8f2d5eb4b0aaae37777ac11b3
ccheung9595 <40210496+ccheung9595@users.noreply.github.com>
2019-12-09 22:32:42 -0600

    Update graph_operations.py

--- graph_operations.py
+++ graph_operations.py
@@ -1,12 +1,16 @@
-def min_cost(G, T):
+
+def min_cost_edge(G, T):
 
     edges_0 = []
-  
+    
+    
+    
     for e in G[1]:
         for v in T[0]:
             if v in e and e not in T[1]:
               edges_0.append(e)
-     
+        
+    
     edges = []
     
     for num in edges_0:
@@ -35,3 +39,9 @@ def min_cost(G, T):
     minimum = [minimum_edge, minimum_weight]
     
     return minimum
+
+def min_cost(minimum):
+    
+    e = minimum[1]
+    
+    return e
