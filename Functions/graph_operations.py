1346f541121d047c8577cc99c234356bc9030ed7
mostt1995 <57509664+mostt1995@users.noreply.github.com>
2019-12-11 11:56:25 +1200

    Update graph_operations.py

--- Functions/graph_operations.py
+++ Functions/graph_operations.py
@@ -21,29 +21,29 @@ def min_cost_edge(G, T):
             
         weights = []
     
-    # finds weights corresponding to edges that have a vertex in the minimum spanning tree graph
+     # finds weights corresponding to edges that have a vertex in the minimum spanning tree graph
     for j in G[1]:
         for i in edges:
             if i == j:
                 weights.append(G[2][G[1].index(j)])
                 
-    # finds minimum weight of the edges that have a vertex in the minimum spanning tree graph
+     # finds minimum weight of the edges that have a vertex in the minimum spanning tree graph
     minimum_weight = weights[0]   
         
     for t in weights:
         if t < minimum_weight:
             minimum_weight = t
     
-    # creates a list with:
-    # the edge with the lowest weight that has a vertex in the minimum spanning tree graph
-    # the corresponding weight of that edge
+     # creates a list with:
+     # the edge with the lowest weight that has a vertex in the minimum spanning tree graph
+     # the corresponding weight of that edge
     minimum_edge = edges[weights.index(minimum_weight)]    
     minimum = [minimum_edge, minimum_weight]
     
     return minimum
 
 def min_cost(minimum):
-    # returns cost of the incident edge with the lowest weight
+     # returns cost of the incident edge with the lowest weight
     
     e = minimum[1]
     
