b3eeea6cd924c4fa7d3587fe4fe638b9f538b910
vishakhamallya <vishakha_mallya@yahoo.com>
2019-12-10 17:54:29 -0600

    Update prims.py

--- Functions/prims.py
+++ Functions/prims.py
@@ -13,10 +13,6 @@ def prims(textfile):
     
     G = get_graph(textfile)
     T_0 = initialize_tree(G)
-
-
-
-
     T = (T_0[0], T_0[1])
     
     minimum_cost = []
@@ -25,28 +21,20 @@ def prims(textfile):
     while len(T[0]) != len(G[0]):
         
         minimum = min_cost_edge(G, T)
-           
         e = min_cost(minimum)
-        
         T[1].append(minimum[0])
-        
         
         vertices = []
         
         for i in range(len(T[1])):
             for j in range(2):
                 vertices.append(T[1][i][j])
-                
-        
+               
         for l in vertices:
             if l not in T[0]:
                 T[0].append(l)
-                
-    
         
         minimum_cost.append(e)
-        
-    
+       
     minimum_graph = (T[0], T[1], minimum_cost)
-    
     return minimum_graph
