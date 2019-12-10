cd2b48c2ca78a0331eb92f80a421c573d9695e60
ccheung9595 <40210496+ccheung9595@users.noreply.github.com>
2019-12-10 03:20:07 -0600

    Update reading_writing_functions.py

--- Functions/reading_writing_functions.py
+++ Functions/reading_writing_functions.py
@@ -6,14 +6,14 @@ def get_graph(textfile):
     import os
     from os.path import dirname, abspath
     
-    #os.path.abspath("graph2.txt")
-    
+    # Sets environment to proper working directory so the .txt file can be uploaded
     d = dirname(abspath("main.py"))
+    directory = d + "\Data"  
+    os.chdir(directory) 
     
-    directory = d + "\Data"
     
-    os.chdir(directory)
-    
+    # loads data from text file into numpy array
+    # whitespace is used to separate the values
     g = np.loadtxt(textfile, usecols=range(3), dtype = int)
     
     vertices = []
@@ -21,54 +21,56 @@ def get_graph(textfile):
     g_column_1 = []
     g_column_2 = []
     
-    #for i in range(0, len(g)-1):
-    #    for j in range(0, len(g[0]-1)):
-     #   glist.insert(g[i][0])
-        
-    
+    # turns columns in numpy array(g) into rows
     for j in range(0, len(g)):
         g_column_0.append(g[j][0])
-      
         
     for i in range(0, len(g)):
         g_column_1.append(g[i][1])
         
     for i in range(0, len(g)):
         g_column_2.append(g[i][2])
-    
+   
+
+    # takes the edges from the graph given
+    # creates a list of all unique vertices in the graph given
     vertices = g_column_0 + g_column_1
-    
     unique_vertices = []
     
     for i in vertices:
         if i not in unique_vertices:
             unique_vertices.append(i)
     
-    
+    # empty list will be used to create new graph for data given
+    # new graph will be used to determine the minimum spanning tree graph
     G = []
     
     edges = []
     
+    # adds a row in the new graph that shows all the vertices in the graph given
     G.insert(0, unique_vertices)
     
-    
+    # adds a row in the new graph for the edges in the graph given
+    # each element in the new row is an edge on the graph given
     for i in range(0, len(g_column_0)):
         add = (g_column_0[i], g_column_1[i])
-        edges.append(add)
-    
+        edges.append(add) 
     edges = [list(elem) for elem in edges]
-    
     G.insert(1, edges)
     
+    # adds a row in the new graph for the corresponding weights of each edge
     G.insert(2, g_column_2)
     
     return G
 
 
 def initialize_tree(G):
-
-    minimum_weight = G[2][0]
     
+    # intializes the minimum_weight variable
+    minimum_weight = G[2][0]
+    # iterates through weights of graph
+    # if the weight is smaller than the initialized minimum weight
+    # then the minimum weight will be replaced with that weight
     for i in range(len(G[2]) - 1):
         if G[2][i] < minimum_weight:
             minimum_weight = G[2][i]
